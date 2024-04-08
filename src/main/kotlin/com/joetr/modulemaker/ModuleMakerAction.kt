f7f092805d9d86f45a5d540389092afe3d357462
Joe Roskopf <7951665+j-roskopf@users.noreply.github.com>
2024-04-08 09:42:40 -0500

    accept starting location

--- src/main/kotlin/com/joetr/modulemaker/ModuleMakerAction.kt
+++ src/main/kotlin/com/joetr/modulemaker/ModuleMakerAction.kt
@@ -4,13 +4,20 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 
 class ModuleMakerAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
         val project: Project = event.getRequiredData(CommonDataKeys.PROJECT)
 
+        val startingLocation: VirtualFile? = event.getData(CommonDataKeys.VIRTUAL_FILE)
+
+        // we only want to use a starting location if it's coming from a directory
+        val shouldUseStartingLocation = startingLocation != null && startingLocation.isDirectory
+
         ModuleMakerDialogWrapper(
-            project = project
+            project = project,
+            startingLocation = if (shouldUseStartingLocation) startingLocation else null
         ).show()
     }
 }
